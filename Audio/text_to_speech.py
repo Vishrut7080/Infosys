a6f09e4a52f2902f6f32e88ff4d9db975392f1cd
Vishrut <165580939+Vishrut7080@users.noreply.github.com>
2026-02-10 22:50:35 +0530

    feat: Add TTS implementation and update dependencies

---
+++ Audio/text_to_speech.py
@@ -0,0 +1,24 @@
+import pyttsx3
+
+def speak_test(text):
+    engine=pyttsx3.init()
+
+    # Custom voice settings
+    engine.setProperty('rate', 160)
+    engine.setProperty('volume', 1.0)
+
+    # Get Avaialble Voices
+    voices=engine.getProperty('voices')
+    engine.setProperty('voice', voices[0])
+
+    # Speech part
+    engine.say(text)
+
+    # Waiting for the speech to end
+    engine.runAndWait()
+
+# sample text to be played
+sample_text='Welcome to the Voice Based Email Assistant.'
+
+# calling function
+speak_test(sample_text)
