8565ae8625a07e9522ac7f3033cfc697caa8197e
Vishrut <165580939+Vishrut7080@users.noreply.github.com>
2026-02-11 11:10:40 +0530

    refactor: main loop with text normalization and exit detection

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from Audio.text_to_speech import speak_text
 from Audio.speech_to_text import listen_text
 
+heard=""
+ending=['goodbye','bye','exit','see you later']
+bye='[Reply]: Goodbye!Take care'
 
-heard=listen_text()
-
+# Open file
 with open('Audio/Transcribe.txt','a') as file:
-    file.write(f'{heard}\n')
+    # loop for repeated audio recordings
+    while True:
+        # listen to audio
+        heard=listen_text()
+        # normalize text
+        clean_heard = heard.lower().strip().replace('.', '')
+
+        # write to file
+        file.write(f'{clean_heard}\n')
+        # check if any ending word is present
+        if any(word in clean_heard for word in ending):
+            print(bye)
+            speak_text(bye)
+            break
+        speak_text(f'{clean_heard}')
     file.close()
-speak_text(f'{heard}')
