e20d44ad5eb0195673d394523e353503f55e1664
Vishrut <165580939+Vishrut7080@users.noreply.github.com>
2026-02-13 18:22:04 +0530

    feat: add greeting command and improve interaction flow

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ send_mail='[System]: This feature will be added in the next milestone.'
 see_inboc='[System]: This feature will be added in the next milestone.'
 ending=['goodbye','bye','exit','see you later']
 bye='[System]: Goodbye!Take care.'
+mail='[System]: Email will be added in next milestone. To be continued....'
+greeting_response='[System]: Hi, what can i do for you?'
 
 # ----------------------
 # Commands
@@ -20,7 +22,7 @@ mail_req=['mail','email','message']
 inbox_req=['inbox','mail','mails']
 affirmation=['yes','ok', 'yah','ya','want to']
 negation=['no', 'nah','nope','don\'t want to']
-mail='[System]: Email will be added in next milestone. To be continued....'
+greeting=['hi','hello''hey']
 
 # ----------------------
 # Command Logic
@@ -32,26 +34,28 @@ with open('Audio/Transcribe.txt','a') as file:
     while True:
         # listen to audio
         heard=listen_text()
-        print(f'[User]: {heard}')
+        speak_text(f'[User]: {heard}')
         # normalize text
         clean_heard = heard.lower().strip().replace('.', '')
 
         # write to file
         file.write(f'{clean_heard}\n')
 
+        if any(word in clean_heard for word in greeting):
+            speak_text(greeting_response)
+
         # To send a mail
-        if 'send' in heard and any(word in clean_heard for word in mail_req):
+        elif 'send' in heard and any(word in clean_heard for word in mail_req):
             speak_text('[System]: You want to send mail?')
             response = listen_text()
             clean_response = response.lower().strip().replace('.', '')
+            speak_text(f'[User]: {clean_response}')
             if any(s in clean_response for s in affirmation):
-                speak_text(clean_response)
                 speak_text(send_mail)
                 compose=open_gmail_compose()
                 speak_text(compose)
                 continue
             elif any(s in clean_response for s in negation):
-                speak_text(clean_response)
                 speak_text('[System]: Ok Thanks for confirming.')
                 continue
 
@@ -60,14 +64,13 @@ with open('Audio/Transcribe.txt','a') as file:
             speak_text('[System]: You want to check the inbox?')
             response = listen_text()
             clean_response = response.lower().strip().replace('.', '')
+            speak_text(f'{clean_response}')
             if any(s in clean_response for s in affirmation):
-                speak_text(clean_response)
                 speak_text(see_inboc)
                 inbox=get_top_senders()
                 speak_text(inbox)
                 continue
             elif any(s in clean_response for s in negation):
-                speak_text(clean_response)
                 speak_text('[System]: Ok Thanks for confirming.')
                 continue
         
